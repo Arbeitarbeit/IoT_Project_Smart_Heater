3d478629da33ab8aac033b166c09b2766a43c201
Yijie Zhang <yijie-zhang@uiowa.edu>
2022-04-23 18:00:49 -0500

    pwm update

--- Raspberry_App/gpio_pwm.js
+++ Raspberry_App/gpio_pwm.js
@@ -4,3 +4,6 @@ const pwm = require('raspi-pwm');
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var Heater = new Gpio(17, 'out'); //use GPIO pin 17, and specify that it is output
 var Fan = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
+
+#
+#
