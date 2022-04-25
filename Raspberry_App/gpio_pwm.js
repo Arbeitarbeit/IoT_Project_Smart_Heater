82f95b487c85158ba9c010966728f1753c187a5a
InterstellarJ2049 <yijie-zhang@uiowa.edu>
2022-04-25 10:30:18 -0500

    pwm and decision logic update

--- Raspberry_App/gpio_pwm.js
+++ Raspberry_App/gpio_pwm.js
@@ -1,3 +1,87 @@
+
+const Gpio = require('pigpio').Gpio;
+
+// const fan = new Gpio(17, {mode: Gpio.OUTPUT});
+// let dutyCycle = 10;
+// while(true){
+//   fan.pwmWrite(dutyCycle);
+// }
+
+function setupGPIO(){
+  const fan0 = new Gpio(17, {mode: Gpio.OUTPUT}); //GPIO17 at Pin11
+  const fan1 = new Gpio(27, {mode: Gpio.OUTPUT}); //GPIO27 at Pin13
+  const heater0 = new Gpio(23, {mode: Gpio.OUTPUT}); //GPIO23 at Pin16
+  const heater1 = new Gpio(24, {mode: Gpio.OUTPUT}); //GPIO24 at Pin18
+  fan0.pwmWrite(0);
+  fan1.pwmWrite(0);
+  heater0.pwmWrite(0);
+  heater1.pwmWrite(0);
+}
+
+function openFan0(){
+  let dutyCycle = 255;
+  fan0.pwmWrite(dutyCycle);
+}
+
+function openFan1(){
+  let dutyCycle = 255;
+  fan1.pwmWrite(dutyCycle);
+}
+
+function openHeater0(){
+  let dutyCycle = 255;
+  heater0.pwmWrite(dutyCycle);
+}
+
+function openHeater1(){
+  let dutyCycle = 255;
+  heater1.pwmWrite(dutyCycle);
+}
+
+// open decision strategy
+
+tempdiff = database['temperature'] - data.temperature
+if(tempdiff < -1)
+if(tempdiff < -1)
+if(tempdiff > 1)
+if(tempdiff > 1)
+
+
+// dutyCycle update depending the temperature difference
+function updateFan0(){
+  let dutyCycle = 150;
+  fan0.pwmWrite(dutyCycle);
+}
+
+function updateFan1(){
+  let dutyCycle = 150;
+  fan1.pwmWrite(dutyCycle);
+}
+
+function updateHeater0(){
+  let dutyCycle = 150;
+  heater0.pwmWrite(dutyCycle);
+}
+
+function updateHeater1(){
+  let dutyCycle = 150;
+  heater1.pwmWrite(dutyCycle);
+}
+
+// update decision strategy
+if(tempdiff > -1||tempdiff < 0)
+if(tempdiff > -1||tempdiff < 0)
+if(tempdiff < 1||tempdiff > 0)
+if(tempdiff < 1||tempdiff > 0)
+
+
+
+
+
+
+
+// test by firebase input:
+
 // const raspi = require('raspi');
 // const pwm = require('raspi-pwm');
 
@@ -23,30 +107,6 @@
 //     dutyCycle = 0;
 //   }
 // }, 20);
-
-
-
-
-const Gpio = require('pigpio').Gpio;
-
-// const fan = new Gpio(17, {mode: Gpio.OUTPUT});
-
-// let dutyCycle = 10;
-
-// while(true){
-//   fan.pwmWrite(dutyCycle);
-// }
-
-function setUpFan(){
-  const fan = new Gpio(17, {mode: Gpio.OUTPUT});
-  fan.pwmWrite(0);
-}
-
-function openFan(){
-  let dutyCycle = 10;
-  fan.pwmWrite(dutyCycle);
-}
-
 
 // setInterval(() => {
 //   fan.pwmWrite(dutyCycle);
