1b4619fc66623e03aaf41f928479143d02209423
InterstellarJ2049 <yijie-zhang@uiowa.edu>
2022-04-24 20:58:26 -0500

    pwm update

--- Raspberry_App/gpio_pwm.js
+++ Raspberry_App/gpio_pwm.js
@@ -9,20 +9,69 @@
 
 // Use PWM to pulse the Fan connected to GPIO12(Pin32) from fully off to fully on continuously.
 
+// const Gpio = require('pigpio').Gpio;
+
+// const fan = new Gpio(12, {mode: Gpio.OUTPUT});
+
+// let dutyCycle = 0;
+
+// setInterval(() => {
+//   fan.pwmWrite(dutyCycle);
+
+//   dutyCycle += 5;
+//   if (dutyCycle > 255) {
+//     dutyCycle = 0;
+//   }
+// }, 20);
+
+
+
+
 const Gpio = require('pigpio').Gpio;
 
-const fan = new Gpio(12, {mode: Gpio.OUTPUT});
+// const fan = new Gpio(17, {mode: Gpio.OUTPUT});
 
-let dutyCycle = 0;
+// let dutyCycle = 10;
 
-setInterval(() => {
+// while(true){
+//   fan.pwmWrite(dutyCycle);
+// }
+
+function setUpFan(){
+  const fan = new Gpio(17, {mode: Gpio.OUTPUT});
+  fan.pwmWrite(0);
+}
+
+function openFan(){
+  let dutyCycle = 10;
   fan.pwmWrite(dutyCycle);
+}
 
-  dutyCycle += 5;
-  if (dutyCycle > 255) {
-    dutyCycle = 0;
-  }
-}, 20);
 
-#
-#
+// setInterval(() => {
+//   fan.pwmWrite(dutyCycle);
+
+// //   dutyCycle += 250;
+// // // dutyCycle = prompt("duty cycle:");
+// //   if (dutyCycle > 255) {
+// //     dutyCycle = 0;
+// //   }
+//   //     dutyCycle = 100;
+//   //   }
+//   //   else {
+//   //     dutyCycle = 255;
+//   //   }
+   
+//   // });
+
+// }, 10000);
+
+
+
+// fan.pwmWrite(dutyCycle);
+
+// await new Promise(resolve => setTimeout(resolve, 1000));
+
+// fan.pwmWrite(100);
+
+
