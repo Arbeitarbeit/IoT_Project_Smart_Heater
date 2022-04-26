bbd1b7035ae0f8bec3d8ed57dc96ac1f2c895ad6
InterstellarJ2049 <yijie-zhang@uiowa.edu>
2022-04-25 21:07:59 -0500

    update app.js

--- Raspberry_App/app.js
+++ Raspberry_App/app.js
@@ -46,7 +46,7 @@ function dispAccel() {
   run_heater_fan();
 
 
-  setTimeout(dispAccel, 5000 - (toc - tic));
+  setTimeout(dispAccel, 2000 - (toc - tic));
 
 
 }
@@ -55,9 +55,9 @@ function writeNewPost(data) {
   const database = getDatabase(app);
   const now = new Date();
 
-  update(ref(database), {
-      "temperature": data.temperature
-  });
+  // update(ref(database), {
+  //     "temperature": data.temperature
+  // });
 
   console.log("Current Temperature: " + data.temperature + "\nHour: " + now.getHours() + ", Minute:" + now.getMinutes());
 }
@@ -65,55 +65,95 @@ function writeNewPost(data) {
 
 
 function run_heater_fan(){
+  var temperature_read;
+  var time_read;
+
   const starCountRef = ref(database, 'update_temp');
     onValue(starCountRef, (snapshot) => {
         const temp_update = snapshot.val();
-
+        
         // if update_light is True
-        if (temp_update){
+        if (!temp_update){
+
           get(child(ref(database), `temperature`)).then((snapshot) => {
             if (snapshot.exists()) 
             {
-              const time_ref = firebase.database().ref('time_Goal');
-              time_ref.on('value',(snap)=>
-              {
-                const time_Goal = snapshot.val();
-                "use strict";
-                console.log("Desired Hour: " + time_Goal["hour"] + "\nDesired Minute: " + time_Goal["minute"]);
-                if ((time_Goal["hour"] < now.getHours()) || (time_Goal["hour"] == now.getHours() && time_Goal["minute"] < now.getMinutes()) || database['temperature'] == data.temperature)
-                {
-                  temp_update = false;
-                  console.log("Invalid Time or Temperature");
-                } else
-                {
-                  if (((time_Goal["hour"] == now.getHours() + 1) && (time_Goal["minute"] == now.getMinutes() - 54)) || (time_Goal["hour"] == now.getHours() && (time_Goal["minute"] == now.getMinutes() + 5)))
-                  {
-                    if (database['temperature'] > data.temperature)
-                    {
-                      //Activate Heater 
-                    }else
-                    {
-                      openFan0();
-                    }
-                    temp_update = false;
-                  }
-                }
-              });
-          } else
-          {
-            console.log("No data available");
-          }
-          update(ref(database), {
-            "update_temp": false,
+              const temparature = snapshot.val();
+              temperature_read = temparature;
+              // var db = app.database();
+              // var time_ref = db.ref("time_Goal");
+              // //var time_ref = firebase.database().ref("time_Goal");
+              // time_ref.once('value',(snap)=>
+              // {
+              //   const time_Goal = snapshot.val();
+              //   "use strict";
+              //   console.log("Desired Hour: " + time_Goal["hour"] + "\nDesired Minute: " + time_Goal["minute"]);
+              //   if ((time_Goal["hour"] < now.getHours()) || (time_Goal["hour"] == now.getHours() && time_Goal["minute"] < now.getMinutes()) || database['temperature'] == data.temperature)
+              //   {
+              //     temp_update = false;
+              //     console.log("Invalid Time or Temperature");
+              //   } else
+              //   {
+              //     if (((time_Goal["hour"] == now.getHours() + 1) && (time_Goal["minute"] == now.getMinutes() - 54)) || (time_Goal["hour"] == now.getHours() && (time_Goal["minute"] == now.getMinutes() + 5)))
+              //     {
+              //       if (database['temperature'] > data.temperature)
+              //       {
+              //         //Activate Heater 
+              //       }else
+              //       {
+              //         openFan0();
+              //       }
+              //       temp_update = false;
+              //     }
+              //   }
+              // });
+            }      
           });
-        }).catch((error) => {
-          console.error(error);
-        });
-      };
+
+          get(child(ref(database), `time_Goal`)).then((snapshot) => {
+            if (snapshot.exists()) {
+              const time_Goal = snapshot.val();
+              time_read = time_Goal;
+            }
+          });
+
+          update(ref(database), {
+            "update_temp": false
+          });
+
+          setTimeout(()=>{ make_action(temperature_read, time_read); }, 80);
+            
+
+        }
     });
 }
 
-
+function make_action(temperature_read, time_read){
+  const now = new Date();
+  var data = IMU.getValueSync();
+  console.log(data.temperature);
+  if ((time_read["hour"] > now.getHours()) && (time_read["hour"] == now.getHours() && time_read["minute"] > now.getMinutes()) || temperature_read != data.temperature)
+  {
+    if (((time_read["hour"] == now.getHours() + 1) && (time_read["minute"] == now.getMinutes() - 54)) || (time_read["hour"] == now.getHours() && (time_read["minute"] == now.getMinutes() + 5)))
+    {
+      if (temperature_read > data.temperature)
+      {
+        console.log("heater");
+      }else
+      {
+        console.log("cooler");
+        // openFan0();
+      }
+      temp_update = false;
+    }
+  }
+  else
+  {
+    temp_update = false;
+    console.log("Invalid Time or Temperature");
+  }
+    
+}
 
 
 
